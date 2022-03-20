42c9273f0cafcd00d75227b9cef9a0de884a983f
菩萨蛮 <higuaifan@higuaifan.com>
2022-03-20 23:23:02 +0800

    🔥 demo代码删除不需要的标签

--- demos/base/button/default.vue
+++ demos/base/button/default.vue
@@ -1,16 +1,3 @@
 <template>
   <w-button>普通按钮</w-button>
 </template>
-
-<script lang="ts" setup>
-/**
- * @Description:
- * @Author: 阿怪
- * @Date: 2022/3/6 1:23 AM
- * @Version v1.0.0
- *
- * 公司的业务千篇一律，复杂的代码好几百行。
- */
-
-
-</script>
--- demos/base/input/default.vue
+++ demos/base/input/default.vue
@@ -1,20 +1,3 @@
 <template>
   <w-input/>
 </template>
-
-<script setup lang="ts">
-/**
- * @Description:
- * @Author: 阿怪
- * @Date: 2022/3/5 1:09 AM
- * @Version v1.0.0
- *
- * 公司的业务千篇一律，复杂的代码好几百行。
- */
-
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
