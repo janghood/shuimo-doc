4017311531ca82b7477a74d16012166bfdbd2aad
higuaifan <higuaifan@higuaifan.com>
2022-04-30 10:03:49 +0800

    ✨ 新增radio文档

---
+++ demos/base/radio/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>选择参数: {{value}}</div>
+  <w-radio v-model="value" label="janghood">极客江湖</w-radio>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const value = ref('');
+</script>
