758715065ad9a95c204e912a630f8ed8a56c4e11
higuaifan <higuaifan@higuaifan.com>
2022-05-02 04:26:19 +0800

    🚀 更新菜单

--- config/menu.config.ts
+++ config/menu.config.ts
@@ -9,13 +9,23 @@
 import type { MenuTypeArr } from "@higuaifan/wash-painting-ui";
 
 
-export const menu:MenuTypeArr = [
+export const menu: MenuTypeArr = [
   { title: '首页', key: 'main', isActive: true },
   { title: '颜色', key: 'docs/color', isActive: false },
   {
     title: '基础组件', key: 'docs/components/base/button', isActive: false, children: [
-      { title: '按钮', key: 'docs/components/base/button', isActive: true },
-      { title: '输入框', key: 'docs/components/base/input', isActive: true },
+      { title: '按钮', key: 'docs/components/base/button', isActive: false },
+      { title: '输入框', key: 'docs/components/base/input', isActive: false },
+      { title: '单选框', key: 'docs/components/base/radio', isActive: false },
+      { title: '复选框', key: 'docs/components/base/checkbox', isActive: false },
+      { title: '选择框', key: 'docs/components/base/select', isActive: false },
+      { title: '日期选择框', key: 'docs/components/base/datePicker', isActive: false },
+      { title: '列表', key: 'docs/components/base/list', isActive: false },
+    ]
+  },
+  {
+    title: '模版组件', key: 'docs/components/template/form', isActive: false, children: [
+      { title: '表单', key: 'docs/components/template/form', isActive: false },
     ]
   }
 ]
