d6231c1e2b698d8a001d51ccc3756dc6c696e12b
higuaifan <higuaifan@higuaifan.com>
2022-11-03 14:20:33 +0800

    🚀 优化checkbox基础demo

--- demos/base/checkbox/default.vue
+++ demos/base/checkbox/default.vue
@@ -1,3 +1,9 @@
 <template>
-  <m-checkbox lable="极客江湖"/>
+  <m-checkbox v-model="value" lable="极客江湖"/>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const value = ref(true);
+</script>
